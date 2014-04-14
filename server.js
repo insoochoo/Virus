a8358cfc23cda398cc487837722f2fbc7dd9140c
Paul <bumj94@gmail.com>
2014-04-14 19:16:56 -0400

    setup for base structure, countdown working

--- server.js
+++ server.js
@@ -7,10 +7,11 @@
 var express=require('express');
 var async=require('async');
 var app=express();
-app.set('port', process.env.PORT || 8080);
-var io = require("socket.io").listen(app.listen(app.get('port')));
+app.set('port', process.env.PORT || 3000);
+var io = require("socket.io").listen(app.listen(app.get('port')) ,{log: false});
 
 console.log("Listening on port http://localhost:" + app.get('port'));
+
 app.use("/static", express.static(__dirname + "/static"));
 
 
@@ -29,9 +30,15 @@ function generateRoom(length) {
 		room += collection.charAt(Math.floor(Math.random() * collection.length));
 	}
 	return room;
-};
+}
 
+function generateTime(){
+	return new Date().getTime();
+}
 
+function generateQuestion(){
+
+}
 /*
 //////////////////////////////////////////////////////////////////
 // ==========================  URL =========================== //
@@ -64,8 +71,9 @@ app.get('/:room{a-zA-Z0-9}{6}', function(req, res){
 		//contains all game instances
 		{
 			*room* : {
-				player1: socket
-				player2: socket
+				player1: socket,
+				player2: socket,
+				roundStartTime: {0-9}
 			}
 		}
 		
@@ -73,13 +81,15 @@ app.get('/:room{a-zA-Z0-9}{6}', function(req, res){
 /* SOCKET:
 		//one player's socket instance
 		{
-			"room" : {a-zA-Z0-9}{6}
-			"opponent" : socket
-			"points" : {0-9}
+			"room" : {a-zA-Z0-9}{6},
+			"opponent" : socket,
+			"points" : {0-9},
+			"ready" : false,
+			"done" : false;
 		}
 */
 
-io.sockets.on("connection",function(){
+io.sockets.on("connection",function(socket){
 	socket.on("join",function(data){
 
 		//initialize socket
@@ -98,6 +108,8 @@ io.sockets.on("connection",function(){
 			socket.set("opponent", games[room].player1);
 			games[room].player1.set("opponent",socket);
 			socket.set("points",0);
+			socket.set("ready", false);
+			socket.set("done", false);
 
 			io.sockets.in(room).emit("notify", { success:1, message:"Both players connected." } );
 		}
@@ -108,10 +120,65 @@ io.sockets.on("connection",function(){
 			socket.join(data.room);
 			socket.set("room", room);
 			socket.set("points",0);
+			socket.set("ready", false);
+			socket.set("done", false);
 		}
 	});
 
+	socket.on("gamestart",function(){
+		async.parallel([
+			socket.get.bind(this, "room"),
+			socket.get.bind(this, "opponent"),
+			socket.get.bind(this, "done")
+	    ], function(err, results) {
 
+	    	socket.set("ready", true);
+
+	    	results[1].get("ready", function(err,ready){
+	    		// if both players are ready
+	    		if(ready){
+
+	    			// countdowns down in the frontend
+	    			socket.emit("countdown", { count : 5 });
+
+	    			// wait for countdown (5,4,3,2,1,start) == 6 seconds
+					setTimeout(function(){
+
+						//start time, generate question, emit it to front end
+						games[results[0]].roundStartTime=generateTime();
+						console.log("START!!!!");
+						//emit (roundLimitTime-(roundNewTime - roundStartTime))/roundLimitTime
+						// roundLimitTime = 5? 10? (sec)
+						/*var stopinterval=setInterval(function(){
+							// if both finished, stop interval
+							// socket.on("done") will set 
+							
+							results[1].get("done", function(err,done){
+								if(done && results[2]){
+									clearInterval(stopinterval);
+								}
+							});
+
+
+						},100);*/
+
+					},6000);
+	    		}
+	    		else{
+	    			socket.emit("notify", { success:0, message:"Waiting on your friend to get ready..."});
+	    			socket.emit("notify", { success:0, message:"Your friend is ready!"});
+	    		}
+	    	});
+	    });
+	});
+
+	socket.on("test",function(){
+		socket.emit("countdown", { count : 5 });
+
+		setTimeout(function(){
+			console.log("hello");
+		},6000);
+	});
 });
 
 
--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -7,9 +7,11 @@ $(document).ready(function(){
 	var socket = io.connect(window.location.hostname);
 
 	socket.on("connect",function(){
+		socket.emit("test");
 		if(room){
 			socket.emit("join",{room:room});
 		}
+
 	});
 
 	socket.on("notify",function(data){
@@ -21,6 +23,19 @@ $(document).ready(function(){
 		}
 		else{ //data.success == -1
 			alertify.error(data.message);
+		}
+	});
+
+	socket.on("countdown",function(data){
+		if(data.count){
+			count=data.count;
+			var stopinterval=setInterval(function(){
+				if(count == 1){
+					clearInterval(stopinterval);
+				}
+				$("p").html(count);
+				count--;
+			},1000);
 		}
 	});
 });
