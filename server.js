2d204a40f3c57da31687d40afa10d927d43eaa17
Insoo Choo <insoo@Insoos-MacBook-Pro.local>
2014-04-15 15:31:33 -0400

    Reset

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-/*
-//////////////////////////////////////////////////////////////////
-// =======================  INITALIZE ======================== //
-////////////////////////////////////////////////////////////////
-*/
-
-var express=require('express');
-var async=require('async');
-var app=express();
+var express = require("express");
+var async = require("async");
+var app = express();
 app.set('port', process.env.PORT || 3000);
 var io = require("socket.io").listen(app.listen(app.get('port')) ,{log: false});
-
-console.log("Listening on port http://localhost:" + app.get('port'));
+ 
+console.log("Listening on port" + app.get('port'));
 
 app.use("/static", express.static(__dirname + "/static"));
 
+var length=6;
 
-/*
-//////////////////////////////////////////////////////////////////
-// =======================  FUNCTIONS ======================== //
-////////////////////////////////////////////////////////////////
-*/
+games={
+	/*
+		*room_id* :{
+			player1: Null //Type: socket
+			player2: Null //Type: socket
 
+		}
+	*/
+}
 
-function generateRoom(length) {
+function generateRoom() {
     var collection = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	var room="";
 
@@ -30,192 +28,437 @@ function generateRoom(length) {
 		room += collection.charAt(Math.floor(Math.random() * collection.length));
 	}
 	return room;
+};
+function initBoard(){
+	var board=[]
+	for (var i=0; i < 6; i ++){
+		row=[]
+		for (var j=0; j < 7; j++){
+			row.push(0);
+		}
+		board.push(row);
+	}
+	return board
 }
 
-function generateTime(){
-	return new Date().getTime();
+function getRow(room, column){
+	var row = 5;
+	// find empty "box" from bottom up
+	while(games[room].board[row][column] != 0 && row > 0){
+		row--;
+	}
+	return row;
 }
 
-function generateQuestion(){
-
+function verifyWinner(count, room, pairs){
+	if(count == 4){
+		console.log(pairs);
+		games[room].player1.emit('gameover', {winner: true, message: "You won!", score:[1,0], highlight:pairs, timeout:1700 });
+		games[room].player2.emit('gameover', {winner: false, message: "You lose!", score:[1,0], highlight:pairs, timeout:1700  });
+		return true;
+	}
+	else if(count == -4){
+		console.log(pairs);
+		games[room].player1.emit('gameover', {winner: false, message: "You lose!", score:[0,1], highlight:pairs, timeout:1700  });
+		games[room].player2.emit('gameover', {winner: true, message: "You won!", score:[0,1], highlight:pairs, timeout:1700  });
+		return true;
+	}
+	return false;
 }
 
-function calculatePoints(){
+function check4(row, column, room){
+	var rStart,rEnd,rStart,cEnd;
+	/*	========================================================== 
+		======================= HORIZONTAL =======================
+		==========================================================  */
+	for(var i=0; i<4; i++){
+		cStart=column-i;
+		cEnd=column+3-i;
 
-}
-/*
-//////////////////////////////////////////////////////////////////
-// ==========================  URL =========================== //
-////////////////////////////////////////////////////////////////
-*/
+		//if 4 boxes within column size
+		if(cStart >= 0 && cEnd < games[room].board[0].length){
+			var count=0;
+			pairs=[]
+			for(var j=cStart; j<=cEnd; j++){
+				count+=games[room].board[row][j];
+				pairs.push([row,j]);
+			}
 
-
-app.get('/', function(req, res){
-	res.render('index.jade');
-});
-
-app.get('/room', function(req, res){
-	room=generateRoom(6);
-	res.render('main.jade', {shareURL: req.host+"/"+room, share: room});
-});
-
-app.get('/:room{a-zA-Z0-9}{6}', function(req, res){
-	room=req.params.room;
-	res.render('main.jade', {shareURL: req.host+"/"+room, share: room});
-});
-
-
-/*
-//////////////////////////////////////////////////////////////////
-// ======================  SOCKETIO ========================== //
-////////////////////////////////////////////////////////////////
-*/
-
-/* GAMES:
-		//contains all game instances
-		{
-			*room* : {
-				player1: socket,
-				player2: socket,
-				roundStartTime: {0-9}
+			if(verifyWinner(count, room, pairs)){
+				return true;
 			}
 		}
-		
-*/
-/* SOCKET:
-		//one player's socket instance
-		{
-			"room" : {a-zA-Z0-9}{6},
-			"opponent" : socket,
-			"points" : {0-9},
-			"ready" : false,
-			"done" : false;
+	}
+	/*	========================================================== 
+		======================= VERTICAL =========================
+		==========================================================  */
+	for(var i=0; i<4; i++){
+		rStart=row-i;
+		rEnd=row+3-i;
+
+		//if 4 boxes within column size
+		if(rStart >= 0 && rEnd < games[room].board.length){
+			var count=0;
+			pairs=[]
+			for(var j=rStart; j<=rEnd; j++){
+				count+=games[room].board[j][column];
+				pairs.push([j,column]);
+			}
+
+			if(verifyWinner(count, room, pairs)){
+				return true;
+			}
 		}
-*/
+	}
+	/*	========================================================== 
+		======================= HORIZONTAL =========================
+		==========================================================  */
+	
+	var steps=[[1,1],[1,-1]]
+	for(var step=0; step < steps.length; step++){
+		var rStep=steps[step][0];
+		var cStep=steps[step][1];
+		for(var i=0; i<4; i++){
+			rStart=row-i*rStep;
+			rEnd=row+(3-i)*rStep;
+			cStart=column-i*cStep;
+			cEnd=column+(3-i)*cStep;
+
+			//if 4 boxes within column size
+			if((rStart >= 0 && rEnd < games[room].board.length)
+				&& (cStart >= 0 && cEnd < games[room].board[0].length)){
+				var count=0;
+				pairs=[]
+
+				for(var j=0; j<4; j++){
+					count+=games[room].board[rStart+(j*rStep)][cStart+(j*cStep)];
+					pairs.push([rStart+(j*rStep),cStart+(j*cStep)]);
+				}
+
+				if(verifyWinner(count, room, pairs)){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function checkDraw(room){
+	//console.log(games[room]['board'][0]);
+	for(var i = 0; i < games[room]['board'][0].length; i ++){
+		//console.log(games[room]['board'][0][i]);
+	    if(games[room]['board'][0][i] == 0)
+	        return false;
+	}
+	return true;
+}
+//URL CONFIG
+app.get("/", function(req, res) {
+    res.render("index.jade");
+});
+app.get("/room", function(req, res) {
+	var room=generateRoom();
+    res.render("main.jade", {shareURL: req.host+"/"+room, share: room});
+});
+app.get("/:room([a-zA-Z0-9]{"+length+"})",function(req,res){
+	room=req.params.room
+    res.render("main.jade", {shareURL: req.host+"/"+room, share: room});
+});
+app.get("/error_full",function(req, res){
+	res.render("full.jade");
+});
+app.get("/exit",function(req, res){
+	res.render("exit.jade");
+});
+app.get("/error_opponent",function(req, res){
+	res.render("exit_forfit.jade");
+});
+app.get("/about",function(req, res){
+	res.render("about.jade");
+});
+
 
 io.sockets.on("connection",function(socket){
+	socket.emit("message",{ me:false, players: false, color: "#bdc3c7", message : "WELCOME! You can chat with your opponent here." });
 	socket.on("join",function(data){
-
-		//initialize socket
-
-		if(room in games){
-
-			//check if 2nd player already exists
-			if(games[room].player2){
-				socket.exit("full");
+		console.log("server room:" + data.room);
+		
+		// Player 2 or more joins the room
+		if(data.room in games){
+			// Validate if room is full or not
+			if(games[data.room].player2)
+			{
+				console.log("3RD PLAYER TRIED TO JOIN THE GAME")
+				socket.emit("kick");
 				return;
 			}
 
-			// initialize player 2
+			// Initiate player 2
 			socket.join(data.room);
-			socket.set("room", room);
-			socket.set("opponent", games[room].player1);
-			games[room].player1.set("opponent",socket);
-			socket.set("points",0);
-			socket.set("ready", false);
-			socket.set("done", false);
-			socket.set("correct", false);
+			socket.set("room", data.room);
+			socket.set("pid", -1);
+			socket.set("color", "#e74c3c");
+			socket.set("preview",[]);
+			socket.set("ready",true);
+			// Set opponents
+			socket.set("opponent", games[data.room].player1);
+			games[data.room].player1.set("opponent", socket);
 
-			io.sockets.in(room).emit("notify", { success:1, message:"Both players connected." } );
+			// Set turn
+			socket.set("turn", false);
+			socket.get("opponent",function(err,opponent){
+				opponent.set("turn",true);
+			});
+
+			// Save player 2 socket into "games" object
+			games[data.room].player2 = socket;
+
+			io.sockets.in(data.room).emit("online");
+
+			console.log("PLAYER 2 HAS JOINED THE GAME");
+			socket.emit("message",{ me: false, players: false, color: "#bdc3c7", message : "Player 1 has joined the game." });
+			io.sockets.in(data.room).emit("message",{ me: false, players: false, color: "#bdc3c7", message : "Player 2 has joined the game." });
+
+			games[data.room].player1.emit("message",{ me:false, players: false, color: "#bdc3c7", message : "It's your turn!" });
+			socket.emit("message",{ me:false, players: false, color: "#bdc3c7", message : "Waiting for your opponent to make a move..." });
+		    		
+			//Notify players
+			games[data.room].player1.emit("notify",{connected:1, turn : true});
+			socket.emit("notify",{connected:1, turn : false});
 		}
-		// Player 1 has joined the room
-		else{
 
-			// initialize player 1
+		// Initiate player 1 and game table
+		else{
+			//Initiate player 1
 			socket.join(data.room);
-			socket.set("room", room);
-			socket.set("points",0);
-			socket.set("ready", false);
-			socket.set("done", false);
-			socket.set("correct", false);
+			socket.set("room", data.room);
+			socket.set("pid", 1);
+			socket.set("color", "#f1c40f");
+			socket.set("turn", false);
+			socket.set("preview", []);
+			socket.set("ready",true);
+			//Initiate game table as an array
+			board=initBoard();
+
+			/*board=[ [ 0, 2, 2, 2, 2, 2, 2 ],
+			  		[ 2, 2, 2, 2, 2, 2, 2 ],
+			  		[ 2, 2, 2, 2, 2, 2, 2 ],
+			  		[ 2, 2, 2, 2, 2, 2, 2 ],
+			  		[ 2, 2, 2, 2, 2, 2, 2 ],
+			  		[ 2, 2, 2, 2, 2, 2, 2 ] ];*/
+			/*board=[ [ 0, 1, 1, 1, 0, 0, 0 ],
+				  	[ 1, 1, 0, 0, 0, 0, 0 ],
+				  	[ 1, 0, 0, 0, 0, 0, 0 ],
+				  	[ 1, 0, 0, 0, 0, 0, 0 ],
+				  	[ -1, 0, 0, 0, 0, 0, 0 ],
+				  	[ -1, 0, 0, 0, 0, 0, 0 ] ];*/
+
+			console.log(board);
+			// initiate "games" object
+			games[data.room]={
+				player1: socket,
+				player2: null,
+				board: board,
+				ended: false
+			}
+			
+			console.log("PLAYER 1 HAS JOINED THE GAME");
+			socket.emit("message",{ me:false, players: false, color: "#bdc3c7", message : "Player 1 has joined the game." });
 		}
 	});
-
-	socket.on("onReady",function(){
+	// check where the player put the "box"
+	socket.on("click",function(data){
 		async.parallel([
-			socket.get.bind(this, "room"),
+			socket.get.bind(this, "turn"),
 			socket.get.bind(this, "opponent"),
-			socket.get.bind(this, "done")
+			socket.get.bind(this, "room"),
+			socket.get.bind(this, "pid"),
+			socket.get.bind(this, "color")
 	    ], function(err, results) {
 
-	    	socket.set("ready", true);
-
-	    	results[1].get("ready", function(err,ready){
-	    		// if both players are ready
-	    		if(ready){
-
-	    			// countdowns down in the frontend
-	    			socket.emit("displayCountdown", { count : 7 });
-
-	    			// wait for countdown (5,4,3,2,1,start) == 6 seconds
-					setTimeout(function(){
-						console.log("START!!!!");
-						//start time, generate question, emit it to front end
+	    	// check if both players are in the game/room
+	    	if(games[results[2]].player2){	
+	    		// check if game is ready to be played
+	    		if(!games[results[2]].ended){
+	    			// check if it is the player's turn
+			    	if(results[0]){
 						
-						games[results[0]].roundStartTime=generateTime();
+			    		// check if column is full
+			    		if(games[results[2]].board[0][data.column] == 0){
+				    		socket.set("turn", false);
+				    		results[1].set("turn", true);
 
-						var elapsedTime, tempTime = 0;
+				    		var row = getRow(results[2], data.column);
 
-						//emit (roundLimitTime-(roundNewTime - roundStartTime))/roundLimitTime
-						// roundLimitTime = 5? 10? (sec)
-						var stopinterval=setInterval(function(){
-							// if both finished, stop interval
-							elapsedTime = tempTime-roundStartTime;
-							displayTime = ((5 - elapsedTime)/5) * 100;
-							if(displayTime < 50){
-								io.sockets.in(results[0]).emit("displayTime", { time : displayTime, color: "#red" });
-							}
-							else{
-								io.sockets.in(results[0]).emit("displayTime", { time : displayTime, color: "#green" });
-							}
-							// socket.on("check") will set 
-							results[1].get("done", function(err,done){
-								
-								// when socket enters a 
-								if(results[2]){
-									finalTime=elapsedTime;
-									socket.emit("displayPoints", { points : calculatePoints(finalTime) });
-									clearInterval(stopinterval);
-								}
-							});
+				    		// occupy empty space as pid
+				    		games[results[2]].board[row][data.column] = results[3];
+				    		
+				    		// drop and display block on frontend
+				    		io.sockets.in(results[2]).emit("drop",{ row:row, column:data.column, color:results[4] });
+				    		//socket.emit("drop",{ row:row, column:data.column, color:results[4] });
+				    		//results[1].emit("drop",{ row:row, column:data.column, color:results[4] });
+				    		
+				    		//console.log("testing " + games[results[2]].board[row][data.column]);
 
-							//
-							tempTime=generateTime();
-
-						},100);
-
-					},8000);
-	    		}
-	    		else{
-	    			socket.emit("notify", { success:0, message:"Waiting on your friend to get ready..."});
-	    			socket.emit("notify", { success:0, message:"Your friend waiting on you!"});
-	    		}
-	    	});
+				    		// check if 4 has been connected
+				    		if(check4(row, data.column, results[2])){
+				    			io.sockets.in(results[2]).emit("message",{ me:false, players: false, color: "#bdc3c7", message : "G A M E &nbsp;O V E R!" });
+				    			games[results[2]].ended=true;
+				    			return;
+				    		}
+				    		else if(checkDraw(results[2])){
+				    			console.log("draw");
+								io.sockets.in(results[2]).emit('gameover', {winner: false, message: "Game was tied", score:[0,0], highlight:[],timeout:300 });
+				    		}
+							results[1].emit("message",{ me:false, players: false, color: "#bdc3c7", message : "It's your turn!" });
+			    			socket.emit("message",{ me:false, players: false, color: "#bdc3c7", message : "Waiting for your opponent to make a move..." });
+				    	}
+				    	else{
+			    			console.log(results[3] + " column is full");
+				    		socket.emit("errorMessage", { message : "The column is full! Try somewhere else." })
+				    	}
+				    }
+			    	else{
+			    		console.log(results[3] + " opponent's turn");
+			    		socket.emit("errorMessage", { message : "Don't be hasty! it's your opponent's turn." });
+			    	}
+			    }
+			    else{
+		    		console.log("not all players are ready");
+			    	socket.emit("notify",{connected:0, turn : results[0]});
+			    }
+		    }
+		    else{
+		    	console.log("player 2 hasn't joined yet");
+		    	socket.emit("errorMessage", { message : "Stop! Your opponent hasn't joined yet." });
+		    }
 	    });
 	});
 
-	socket.on("check",function(){
-		
-		if(room in games){
-			socket.set("done", true);
+	socket.on("hover",function(data){
+		async.parallel([
+			socket.get.bind(this, "room"),
+			socket.get.bind(this, "color"),
+			socket.get.bind(this, "opponent"),
+			socket.get.bind(this, "preview")
+	    ], function(err, results) {
 
+			if(results[0] in games){
+			
+				// on mouseenter
+			    if(data.hover == 1){
+			    	var row=getRow(results[0], data.column);
+			    	socket.set("preview", [row, data.column]);
+			    	io.sockets.in(results[0]).emit("preview",{ hover:1, row:row, column:data.column, color:results[1] });
+			    }
 
-		}
+			    // on mouseleave
+			    else{
+			    	io.sockets.in(results[0]).emit("preview",{ hover:0, row:results[3][0], column:results[3][1], color:results[1] });
+			    	if(games[results[0]].player2){
+				    	results[2].get("preview", function(err, preview) {
+				    	
+			    			//if row and column are the same
+			    			//if both players hovering over the same block
+			    			if(results[3][0] == preview[0]
+			    				&& results[3][1] == preview[1]){
+			    				results[2].get("color", function(err, color) {
+			    				//re-color whoever is still hovering over the same block again
+			    					io.sockets.in(results[0]).emit("preview",{ hover:1, row:preview[0], column:preview[1], color:color });
+			    				});
+			    				socket.set("preview", []);
+			    			}
+				    	});
+					}
+			    
+			    	
+			    }
+			}
+	    });
 	});
 
-	socket.on("test",function(){
-		socket.emit("countdown", { count : 5 });
-
-		setTimeout(function(){
-			console.log("hello");
-		},6000);
+	// ready is reset
+	// players ready becomes true when user clicks "ok" when asked to play again
+	socket.on("reset_ready",function(){
+		socket.set("ready", false);
 	});
+
+	socket.on("reset",function(){
+		async.parallel([
+			socket.get.bind(this, "turn"),
+			socket.get.bind(this, "room"),
+			socket.get.bind(this, "opponent")
+	    ], function(err, results) {
+
+	    	if(results[1] in games){
+				//re-initialize board
+				board=initBoard();
+		    	games[results[1]].board=board;
+				socket.set("ready", true);
+
+	    		// to check if opponent is also ready
+	    		results[2].get("ready",function(err, ready){
+	    			
+	    			// if your opponent is ready, reset game
+	    			if(ready){
+	    				console.log("ready");
+				    	games[results[1]].ended=false;
+
+				    	if(results[0]){
+							socket.emit("message",{ me:false, players: false, color: "#bdc3c7", message : "It's your turn!" });
+			    			results[2].emit("message",{ me:false, players: false, color: "#bdc3c7", message : "Waiting for your opponent to make a move..." });
+				    	}
+				    	else{
+							results[2].emit("message",{ me:false, players: false, color: "#bdc3c7", message : "It's your turn!" });
+			    			socket.emit("message",{ me:false, players: false, color: "#bdc3c7", message : "Waiting for your opponent to make a move..." });
+				    	}
+				    	// notify current socket player
+				    	socket.emit("notify",{connected:1, turn : results[0]});
+	    				
+	    				// notify opponent
+	    				results[2].get("turn",function(err, turn){
+				    		results[2].emit("notify",{connected:1, turn : turn});
+				    	});
+				    }
+		    	});
+		    }
+	    });
+	});
+
+	socket.on("disconnect",function(){
+		socket.get("room",function(err,room){
+	    	if(room in games){
+				console.log("Disconnecting...");
+				io.sockets.in(room).emit('leave');
+				if(room in games){
+					delete games.room;
+				}
+			}
+		});
+	});
+
+
+	socket.on("send",function(data){
+		async.parallel([
+			socket.get.bind(this, "room"),
+			socket.get.bind(this, "color"),
+			socket.get.bind(this, "opponent")
+	    ], function(err, results) {
+			if(results[0] in games){
+				if(results[2]){
+					results[2].emit("message",{ me:false, players: true, color: results[1], message : data.message });
+				}
+				socket.emit("message",{ me:true, players: true, color: results[1], message : data.message });
+			}
+		});
+	});
+
 });
 
 
 
-
-
+	
 
 
 
--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -1,41 +1,251 @@
+$(document).ready(function() {
+    var content="";
+    for(var i = 0; i < 6; i++){
+        content+="<tr>";
+        for(var j = 0; j < 7; j++) {
+          content+="<td class='box' data-row="+i+" data-column="+j+"><i class='fa fa-circle'>";
+        }
+    }
+    $("#board table").append(content);
+
+	var clip = new ZeroClipboard( 
+		document.getElementById('copy-clipboard'), {
+		moviePath: "static/flash/ZeroClipboard.swf"
+	});
+
+
+	clip.on( 'complete', function(client, args){
+		alertify.log("Your room URL has been copied. Send it to your friend!");
+	});
+
+	$("#help-icon").tooltip({
+	'selector': '',
+	'placement': 'top',
+	'container':'body'
+	});
+	$("#send_message").tooltip({
+	'selector': '',
+	'placement': 'left',
+	'container':'body'
+	});
+	/*$(zero.htmlBridge).tooltip({
+		 	"rel" : "tooltip",
+		 	"data-toggle" : "tooltip",
+		 	"data-placement" : "right",
+		 	"title" : "Click me to copy the URL and send to your friend !"
+	});*/
+});
+
+var socket = io.connect(window.location.hostname);
+
+
 /*
 //////////////////////////////////////////////////////////////////
-// ======================  SOCKETIO ========================== //
+// ==========================  GAME ========================== //
 ////////////////////////////////////////////////////////////////
 */
-$(document).ready(function(){
-	var socket = io.connect(window.location.hostname);
 
-	socket.on("connect",function(){
-		socket.emit("test");
-		if(room){
-			socket.emit("join",{room:room});
-		}
+var room = $("input").data("room");
 
-	});
-
-	socket.on("notify",function(data){
-		if(data.success == 1){
-			alertify.success(data.message);
-		}
-		else if(data.success == 0){
-			alertify.log(data.message);
-		}
-		else{ //data.success == -1
-			alertify.error(data.message);
-		}
-	});
-
-	socket.on("displayCountdown",function(data){
-		if(data.count){
-			count=data.count;
-			var stopinterval=setInterval(function(){
-				if(count == 1){
-					clearInterval(stopinterval);
-				}
-				$("p").html(count);
-				count--;
-			},1000);
-		}
-	});
+socket.on("connect",function(){
+	if(room){
+		socket.emit("join",{room:room});
+	}
 });
+
+socket.on("kick",function(data){
+	window.location = "/error_full";
+});
+
+socket.on("online",function(){
+	$('.p2-status i').css("color","#2ecc71");
+	$('.p2-status span').html(" Connected");
+});
+
+socket.on("notify",function(data){
+	if(data.connected == 1){
+		if(data.turn){
+			alertify.success("Both players connected. Your turn!");
+		}
+		else{
+			alertify.success("Both players connected. Opponent's turn!");
+		}
+	}
+	else{
+		alertify.log("Waiting on your opponent's response.");
+	}
+});
+
+
+
+$(document).ready(function() {
+	$(".box").click(function(){
+		socket.emit("click", { row : $(this).data("row"), column : $(this).data("column") });
+	});
+	var count=0;
+
+	$(".box").mouseenter(function(){
+		socket.emit("hover", {hover:1, row : $(this).data("row"), column : $(this).data("column") })
+	});
+	$(".box").mouseleave(function(){
+		socket.emit("hover", {hover:0, row : $(this).data("row"), column : $(this).data("column") })
+	});
+
+});
+
+socket.on("drop",function(data){
+	var row = 0;
+	var stopinterval=setInterval(function(){
+		if(row == data.row){
+			clearInterval(stopinterval);
+		}
+		//color in one at a time
+		$(".box[data-row='"+(row-1)+"'][data-column='"+data.column+"'] i").css("color",'');
+		$(".box[data-row='"+row+"'][data-column='"+data.column+"'] i").css("color",data.color);
+		$(".box[data-row='"+row+"'][data-column='"+data.column+"'] i").css("opacity",1);
+		row++;
+
+	},25);
+});
+
+socket.on("preview",function(data){
+	var box_object = $(".box[data-row='"+data.row+"'][data-column='"+data.column+"'] i");
+	
+	if(box_object.css("opacity") != 1
+		|| box_object.css("color") == "rgb(217, 220, 222)"){
+		if(data.hover == 1){
+			box_object.css("color",data.color);
+			box_object.css("opacity",0.3);
+		}
+		else{
+			box_object.css("color","");
+			box_object.css("opacity",1);
+		}
+	}
+});
+
+socket.on("gameover",function(data){
+	socket.emit("reset_ready");
+	var count=0;
+	setTimeout(function(){
+		var stopinterval=setInterval(function(){
+			if(count == 4){
+				clearInterval(stopinterval);
+			}
+			//alert(data.highlight[count]);
+			pair=data.highlight[count];
+			$(".box[data-row='"+pair[0]+"'][data-column='"+pair[1]+"'] i").css("color","#2ecc71");
+			count++;
+		},200);
+ 	},150);
+	p1=parseInt($(".p1-score span").html())+data.score[0];
+	p2=parseInt($(".p2-score span").html())+data.score[1];
+	$(".p1-score span").html(p1);
+	$(".p2-score span").html(p2);
+
+	setTimeout(function(){
+		alertify.set({ labels: {
+		    ok     : "Play again!",
+		    cancel : "Leave Room"
+		} });
+		alertify.confirm(data.message, function(e){
+			if(e) {
+				$("td i").css("color","");
+	            socket.emit("reset");
+	        }
+	        else {
+	       		socket.emit("disconnect");
+	            window.location = '/exit';
+	        }
+		}, 'confirm');
+	},data.timeout);
+});
+
+socket.on("leave",function(){
+	window.location = '/error_opponent';
+});
+
+socket.on("errorMessage",function(data){
+	alertify.error(data.message);
+});
+
+/*
+//////////////////////////////////////////////////////////////////
+// ========================  MESSAGES ======================== //
+////////////////////////////////////////////////////////////////
+*/
+
+function sendMessage(){
+		socket.emit("send", { message : $('.field').val() });
+		$('.field').val("");
+}
+
+$(document).ready(function(){
+	$('.send').click(function(){
+		sendMessage();
+	});
+	$(".field").keypress(function(e) {
+        if(e.which == 13) {
+            sendMessage();
+        }
+    });
+    /*$(document).bind('keydown',function(e){
+    	$('.field').focus();
+    	$(document).unbind('keydown');
+	});*/
+});
+
+var messages = [
+		/*{
+			players:false,
+			color: HEX,
+			message: TEXT
+		}*/
+];
+
+
+socket.on('message',function(data){
+	if(data.message){
+		messages.push(
+			{
+				me:data.me,
+				players:data.players,
+				color: data.color,
+				message: data.message
+			});
+
+		var content="";
+		for(var i = 0; i < messages.length; i ++){
+			if(messages[i].players){
+				var display="";
+				if(messages[i].me){
+					display="Me";
+				}
+				else{
+					display="Opponent";
+				}
+				content+= "<span style='letter-spacing: 0.7px;'><span style='color:"+messages[i].color+"'>"+display+"</span> : "+messages[i].message+"</span><br/>";
+				
+			}
+			else{
+
+				content+= "<span style='letter-spacing: 0.7px; color:"+messages[i].color+"'>"+messages[i].message+"</span><br/>";
+			}
+		}
+		$('#content').html(content);
+		$("#content").scrollTop($("#content")[0].scrollHeight);
+		$('.field').focus();
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
